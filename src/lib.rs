6ce476630bc6b4b0cd9abe3d59dcfde2582ca735
Wendell Smith <wackywendell@gmail.com>
2015-09-11 10:47:42 -0400

    Added documentation option to macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,21 @@
 #[macro_export]
 macro_rules! configurator {
     ($config_name:ident {
-        $($attr_fixed:ident : $attr_fixed_type:ty),*;
-        $($attr_def:ident : $attr_def_type:ty = $def:expr),*
+        $($attr_fixed:ident : $attr_fixed_type:ty, $fixed_doc:expr),*;
+        $($attr_def:ident : $attr_def_type:ty = $def:expr, $def_doc:expr),*
     }) => {
         #[derive(Debug)]
-        struct $config_name {
-            $($attr_fixed : $attr_fixed_type),*,
-            $($attr_def : Option<$attr_def_type>),*
-        }
-    }
-}
-
-
-macro_rules! config {
-    ($config_name:ident {
-        $attr_fixed:ident : $attr_fixed_type:ty
-    }) => {
-        #[derive(Debug)]
-        struct $config_name {
-            $attr_fixed : $attr_fixed_type,
+        pub struct $config_name {
+            $(#[doc = $fixed_doc] pub $attr_fixed : $attr_fixed_type),*,
+            $(#[doc = $def_doc] pub $attr_def : Option<$attr_def_type>),*
         }
     }
 }
 
 configurator!(
     MyConfig {
-        x : isize;
-        y : isize = 4
+        x : isize, "x option";
+        y : isize = 4, "y option"
     }
 );
 
@@ -38,4 +26,6 @@ fn it_works() {
         y: Some(4)
     };
     println!("{:?}", c);
+    
+    assert!(false);
 }
